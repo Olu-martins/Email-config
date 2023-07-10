b5fce28994c8fdfd44d066290a72d69eaa4c9291
olu-martins <mayowasamuel86@gmail.com>
2023-07-10 15:17:13 +0100

    deleted package lock

--- .react-email/.next/server/flight-server-css-manifest.js
+++ .react-email/.next/server/flight-server-css-manifest.js
@@ -1,24 +1,3 @@
 self.__RSC_CSS_MANIFEST={
-  "__entry_css_mods__": {
-    "/Users/mac/Documents/Kora-frontend/REACT EMAIL/react-email-starter/.react-email/src/app/preview/[slug]/page": [
-      "/Users/mac/Documents/Kora-frontend/REACT EMAIL/react-email-starter/.react-email/src/styles/globals.css",
-      "/Users/mac/Documents/Kora-frontend/REACT EMAIL/react-email-starter/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}"
-    ],
-    "/Users/mac/Documents/Kora-frontend/REACT EMAIL/react-email-starter/.react-email/src/app/page": [
-      "/Users/mac/Documents/Kora-frontend/REACT EMAIL/react-email-starter/.react-email/src/styles/globals.css",
-      "/Users/mac/Documents/Kora-frontend/REACT EMAIL/react-email-starter/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}"
-    ]
-  },
-  "/Users/mac/Documents/Kora-frontend/REACT EMAIL/react-email-starter/.react-email/src/app/layout.tsx": [
-    "/Users/mac/Documents/Kora-frontend/REACT EMAIL/react-email-starter/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
-    "/Users/mac/Documents/Kora-frontend/REACT EMAIL/react-email-starter/.react-email/src/styles/globals.css"
-  ],
-  "/Users/mac/Documents/Kora-frontend/REACT EMAIL/react-email-starter/.react-email/src/app/preview/[slug]/page.tsx": [
-    "/Users/mac/Documents/Kora-frontend/REACT EMAIL/react-email-starter/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
-    "/Users/mac/Documents/Kora-frontend/REACT EMAIL/react-email-starter/.react-email/src/styles/globals.css"
-  ],
-  "/Users/mac/Documents/Kora-frontend/REACT EMAIL/react-email-starter/.react-email/src/app/page.tsx": [
-    "/Users/mac/Documents/Kora-frontend/REACT EMAIL/react-email-starter/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
-    "/Users/mac/Documents/Kora-frontend/REACT EMAIL/react-email-starter/.react-email/src/styles/globals.css"
-  ]
+  "__entry_css_mods__": {}
 }
--- .react-email/.next/static/webpack/webpack.50df217d57cb2284.hot-update.js
+++ .react-email/.next/static/webpack/webpack.50df217d57cb2284.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "212789ac78c68ac2"; }
+/******/ 	__webpack_require__.h = function() { return "2d3bedc2d38f2b24"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
